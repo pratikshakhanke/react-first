4b3bb2a634d937982cd538ae842b634cffb8469f
pratikshakhanke <pratikshakhanke1@gmail.com>
2021-12-02 14:21:20 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './style.css';
+import A from './a';
+import B from './b';
+import C from './c';
+import ButtonContext from './ButtonContext';
 
 export default function App() {
+  const text = 'Button';
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>I am learning react</h1>
+      <ButtonContext.Provider value={{ text }}>
+        <A />
+        <B />
+        <C />
+      </ButtonContext.Provider>
     </div>
   );
 }
